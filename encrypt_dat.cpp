0a98a52bb9bbf3383e3586211f94310c66949e6b
amkhan02 <ameenmkhan02@gmail.com>
2024-09-20 13:45:05 -0400

    added comments to code

--- encrypt_dat.cpp
+++ encrypt_dat.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using namespace std::chrono;
 
+// Function to split a string into a vector of strings based on a specified delimiter
 vector<string> split(string& s, string& delimiter){
     vector<string> substrings;
     size_t pos = 0;
@@ -27,26 +28,31 @@ int main(){
     string delimiter = ",";
     ifstream dataFile ("Homework_02_Fall24.DAT");
     ofstream cipherFile ("ENCRYPTED.dat");
-    double e = 2.0;
+    double e = 2.0; // TODO: read in an RSA key pair from a generated file
+                    // * Will need to convert from base64
+                    // * Decide on a key format (start with openSSH, expand to different formats as necessary)
     double n = 1;
     double total_time = 0;
     
     if (dataFile.is_open()){
         while (getline(dataFile, line)){
-            auto start = high_resolution_clock::now();
+            auto start = high_resolution_clock::now(); // Start timer
             if (cipherFile.is_open()){
                 
                 vector<string> items = split(line, delimiter);
                 
+                // Placeholder for encryption
+                // This code simply doubles the voltage, current, and frequency data
+                // TODO: replace this with an RSA calculation 
                 for (int i = 0 ; i < items.size(); ++i)
                     cipherFile<<stoi(items[i]) * (i > 1 ? e : 1)<<' ';
                 
                 cipherFile<<'\n';
             } else
                 break;
-            auto stop = high_resolution_clock::now();
-            auto duration = duration_cast<microseconds>(stop - start);
-            total_time = duration.count();
+            auto stop = high_resolution_clock::now(); // Stop timer
+            auto duration = duration_cast<microseconds>(stop - start); // Calculate elapsed time
+            total_time = duration.count(); // save to print at the end
         }
         if (cipherFile.is_open())
             cipherFile.close();
@@ -56,7 +62,7 @@ int main(){
 
     
     
-    cout << total_time/1000000;
+    cout << total_time/1000000; // print the measured time in seconds
 
     return 0;
 }
